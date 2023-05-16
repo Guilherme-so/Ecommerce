3c1fcdb14e7aacc96881489fd71821a83d9eef24
Guilherme <guilhermemagno09@gmail.com>
2023-05-16 13:23:57 -0300

    creating navbar

---
+++ src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Header, Wrapper, Logo, NavLink, NavStyled } from "./styled";
+import Center from "../center";
+
+export default function Navbar() {
+  return (
+    <Header>
+      <Center>
+        <Wrapper>
+          <Logo href="/">Ecommerce</Logo>
+          <NavStyled>
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/products">All Products</NavLink>
+            <NavLink href="/categories">Categories</NavLink>
+            <NavLink href="/account">Account</NavLink>
+            <NavLink href="/cart">Cart (0)</NavLink>
+          </NavStyled>
+        </Wrapper>
+      </Center>
+    </Header>
+  );
+}
---
+++ src/components/header/styled.ts
@@ -0,0 +1,28 @@
+import { styled } from "styled-components";
+import Link from "next/link";
+
+export const Header = styled.header`
+  background-color: #000;
+  padding: 20px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Logo = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  font-size: large;
+`;
+
+export const NavStyled = styled.nav`
+  display: flex;
+  gap: 10px;
+`;
+
+export const NavLink = styled(Link)`
+  text-decoration: none;
+  color: gray;
+`;
