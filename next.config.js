c23b85098ca71204920620908b156b64a9e6095e
Guilherme <guilhermemagno09@gmail.com>
2023-05-16 20:58:48 -0300

    adding mongoose and fixing styled-components error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  // add the following snippet
   compiler: {
     styledComponents: true,
   },
