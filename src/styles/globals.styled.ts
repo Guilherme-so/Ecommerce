286f9317b3fd44613f14d1b342b685e9d17c948a
Guilherme <guilhermemagno09@gmail.com>
2023-05-16 13:26:23 -0300

    adding styled-components and config it

---
+++ src/styles/globals.styled.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+}
+
+`;
