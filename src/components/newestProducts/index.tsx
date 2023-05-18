947c4e3348aadaa30d7ae3d58ce08ece26688e12
Guilherme <guilhermemagno09@gmail.com>
2023-05-17 23:32:28 -0300

    adding scrollIntoView to scroll to product id

---
+++ src/components/newestProducts/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from "react";
+import Center from "../center";
+import { Container } from "./styled";
+import Cardbox from "../newestCard";
+import { useRouter } from "next/router";
+
+export default function NewestProducts({ newest }) {
+  const myRef = useRef(null);
+  const router = useRouter();
+  const { asPath } = router;
+
+  const executeScroll = () =>
+    myRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+
+  useEffect(() => {
+    if (asPath === "/#products") {
+      executeScroll();
+    }
+  }, []);
+
+  return (
+    <Center>
+      <Container ref={myRef}>
+        {newest?.map((product) => (
+          <Cardbox key={product._id} product={product} />
+        ))}
+      </Container>
+    </Center>
+  );
+}
---
+++ src/components/newestProducts/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  justify-content: center;
+  gap: 40px;
+  margin: 30px;
+  scroll-margin-top: +100px;
+`;
