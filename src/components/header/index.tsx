d5f1a9ae86fb4b3638d80cc7da018676bb08c2f3
Guilherme <guilhermemagno09@gmail.com>
2023-05-21 21:59:05 -0300

    adding favorites

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { Container, Wrapper, Logo, NavLink, NavStyled } from "./styled";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/redux/store";
 import Center from "../center";
 import { Handbag } from "phosphor-react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
+import ChevronRight from "../icons/chevronRight";
+import {
+  Container,
+  Wrapper,
+  Logo,
+  NavLink,
+  NavStyled,
+  Products,
+  ImageContainer,
+  Info,
+  VerTodos,
+} from "./styled";
+import Image from "next/image";
+import Button from "../buttons";
+import { removeToFavorites } from "@/redux/features/favorites/favoritesSlice";
 
 export default function Navbar() {
   const cart = useSelector((state: RootState) => state.cart.value);
+  const favorites = useSelector((state: RootState) => state.favorites.value);
+  const dispatch = useDispatch();
+  const [openFavoritos, setOpenFavoritos] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  console.log(products);
+
+  const fetchFavorites = async () => {
+    const data = {
+      favorites: favorites ?? favorites,
+    };
+    const resp = await axios.post("/api/favorites", data);
+    setProducts(resp.data);
+  };
+
+  useEffect(() => {
+    fetchFavorites();
+  }, [favorites]);
 
   return (
     <Container>
@@ -14,10 +47,82 @@ export default function Navbar() {
         <Wrapper>
           <Logo href="/">ARGO</Logo>
           <NavStyled>
-            <NavLink href="/">Home</NavLink>
+            <NavLink href="/account">Account</NavLink>
             <NavLink href="/products">All Products</NavLink>
             <NavLink href="/categories">Categories</NavLink>
-            <NavLink href="/account">Account</NavLink>
+            <NavLink href="#" onMouseMove={() => setOpenFavoritos(true)}>
+              Favoritos
+              <span>
+                <ChevronRight />
+              </span>
+              {openFavoritos && (
+                <div
+                  className="favoritos"
+                  onMouseLeave={() => setOpenFavoritos(false)}
+                >
+                  <span className="carot"></span>
+                  <h2>Favoritos</h2>
+                  <hr />
+                  <div className="test">
+                    {products?.map((product) => (
+                      <Products key={product._id}>
+                        <ImageContainer>
+                          <Image
+                            src={product.images[0]}
+                            alt={product.title}
+                            height={50}
+                            width={50}
+                          />
+                        </ImageContainer>
+                        <Info>
+                          <h1>{product.title}</h1>
+                          <div>
+                            <span className="price">
+                              {product.price.toLocaleString("pt-BR", {
+                                style: "currency",
+                                currency: "BRL",
+                              })}
+                            </span>
+                            <div>
+                              <p className="parcela">
+                                em{" "}
+                                <span>
+                                  10x{" "}
+                                  {(product.price / 10).toLocaleString(
+                                    "pt-BR",
+                                    {
+                                      style: "currency",
+                                      currency: "BRL",
+                                    }
+                                  )}{" "}
+                                </span>
+                                <span>sem juros</span>
+                              </p>
+                            </div>
+                            <Button
+                              onClick={() => {
+                                dispatch(
+                                  removeToFavorites(product._id.toString())
+                                );
+                                fetchFavorites();
+                              }}
+                              btntype="outline-white"
+                            >
+                              Remover
+                            </Button>
+                          </div>
+                        </Info>
+                      </Products>
+                    ))}
+                  </div>
+                  {products.length > 0 && (
+                    <VerTodos>
+                      <h3>Ver todos</h3>
+                    </VerTodos>
+                  )}
+                </div>
+              )}
+            </NavLink>
             <NavLink href="/cart" cartlength={cart.length > 9 ? true : false}>
               <Handbag size={30} className="cart" color="#474637" />
               <span className="count">{cart.length}</span>
--- src/components/header/styled.ts
+++ src/components/header/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Link from "next/link";
 
 export const Container = styled.div`
-  background-color: #FFF159;
+  background-color: #fff159;
   padding: 20px;
   position: fixed;
   top: 0;
@@ -13,6 +13,7 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 export const Logo = styled(Link)`
@@ -25,24 +26,143 @@ export const Logo = styled(Link)`
 export const NavStyled = styled.nav`
   display: flex;
   gap: 10px;
+  align-items: center;
 `;
 
 export const NavLink = styled(Link)<{ cartlength?: boolean }>`
+  position: relative;
   text-decoration: none;
   color: #474637;
   position: relative;
 
+  span {
+    svg {
+      translate: 2px 4px;
+      rotate: 90deg;
+    }
+  }
+
   .cart {
-    translate: 0px -8px;
+    translate: 0px -2px;
   }
 
   .count {
     position: absolute;
-    top: 4px;
-    left: ${({cartlength})=> cartlength ? '9px' : '13px'  };
+    top: 10px;
+    left: ${({ cartlength }) => (cartlength ? "9px" : "13px")};
     /* left: 13px; */
     color: #474637;
     font-size: x-small;
     font-weight: bold;
   }
+
+  .favoritos {
+    position: absolute;
+    top: 41px;
+    right: 0px;
+    background: white;
+    width: 400px;
+    border-radius: 4px;
+    -webkit-box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.75);
+
+    h2 {
+      margin: 10px 30px;
+    }
+
+    .test {
+      overflow: hidden;
+      height: 180px;
+      overflow-y: hidden;
+      overflow-y: scroll;
+
+      ::-webkit-scrollbar {
+        width: 10px;
+      }
+
+      /* Track */
+      ::-webkit-scrollbar-track {
+        background: none;
+      }
+
+      /* Handle */
+      ::-webkit-scrollbar-thumb {
+        background: #dadada;
+        border-radius: 2px;
+      }
+    }
+
+    .carot {
+      border: 9px solid transparent;
+      border-bottom-color: white;
+      position: absolute;
+      top: -17px;
+      right: 20px;
+    }
+
+    h2 {
+      font-size: medium;
+      margin-bottom: 5px;
+    }
+
+    hr {
+      opacity: 0.5;
+    }
+  }
+`;
+
+export const Products = styled.div`
+  display: flex;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const ImageContainer = styled.div`
+  padding: 5px 20px;
+`;
+
+export const Info = styled.div`
+  h1 {
+    font-size: medium;
+    margin-bottom: 7px;
+  }
+
+  .price {
+    font-size: 20px;
+    font-weight: 100;
+    opacity: 0.8;
+  }
+
+  .desc {
+    h4 {
+      opacity: 0.8;
+      margin-bottom: 5px;
+    }
+    p {
+      font-size: small;
+      opacity: 0.7;
+    }
+  }
+
+  .parcela {
+    margin-top: 2px;
+    span {
+      color: #35a751;
+      font-weight: 500;
+      font-size: 18px;
+    }
+  }
+`;
+
+export const VerTodos = styled.div`
+  width: 100%;
+  display: flex;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  -webkit-box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.75);
+
 `;
