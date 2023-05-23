0702afa2d0699ad53e8e4290fed187adb54bb98e
Guilherme <guilhermemagno09@gmail.com>
2023-05-23 15:44:32 -0300

    creating footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styled";
+
+export default function Footer() {
+  return (
+    <Container>
+      <h3>&copy; Ecommerce 2023 All Rights Reserved</h3>
+    </Container>
+  );
+}
---
+++ src/components/footer/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+width: 100%;
+height: 60px;
+background-color: #FEE456;
+display: flex;
+align-items: center;
+justify-content: center;
+
+h3{
+    font-size: medium;
+    opacity: .7;
+}
+`
